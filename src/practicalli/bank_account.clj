5c940eeab70ce674807497e1b07442fc790e929b
John Practicalli Stevenson <john@jr0cket.co.uk>
2020-06-07 14:41:47 +0100

    bank-account test data in a comment expression
    Ensure test data is not run accidentally by wrapping those expressions in a
    comment expression.

--- src/practicalli/bank_account.clj
+++ src/practicalli/bank_account.clj
@@ -29,17 +29,22 @@
 ;; Test data
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+(comment
 
-;; failing specification test
-(register-account-holder {:first-name          "Jenny"
-                          :last-name           "Jetpack"
-                          :email-address       "jenny@jetpack.org"
-                          :residential-address "42 meaning of life street"
-                          :postal-code         "AB3 0EF"})
+  ;; failing specification test
+  (register-account-holder {:first-name          "Jenny"
+                            :last-name           "Jetpack"
+                            :email-address       "jenny@jetpack.org"
+                            :residential-address "42 meaning of life street"
+                            :postal-code         "AB3 0EF"})
 
-(register-account-holder {:first-name          "Jenny"
-                          :last-name           "Jetpack"
-                          :email-address       "jenny@jetpack.org"
-                          :residential-address "42 meaning of life street"
-                          :postal-code         "AB3 0EF"
-                          :social-security-id  "123456789"})
+  (register-account-holder {:first-name          "Jenny"
+                            :last-name           "Jetpack"
+                            :email-address       "jenny@jetpack.org"
+                            :residential-address "42 meaning of life street"
+                            :postal-code         "AB3 0EF"
+                            :social-security-id  "123456789"})
+
+
+
+  ) ;; End of comment
